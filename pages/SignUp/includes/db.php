f79c9b3fe59108d1dcfd1de864fab583f2418fed
Ivanlizardo <ivanmarklizardoup@gmail.com>
2026-07-09 13:22:04 +0800

    feat: Add database connection files for SignIn and SignUp pages

---
+++ pages/SignUp/includes/db.php
@@ -0,0 +1,21 @@
+<?php
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "admin_db";
+
+    $conn = new mysqli(
+        $servername,
+        $username,
+        $password,
+        $database
+    );
+
+    if ($conn->connect_error) {
+
+        die("Connection failed: " . $conn->connect_error);
+
+    }
+
+?>
